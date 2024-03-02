d07459357c29b0d62fdb7a6ec0f93f69d2a4bc90
diegopalencia15 <diegopalencia15@hotmail.com>
2024-03-01 21:03:40 -0500

    vista_1.1

---
+++ vista/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def inicio(request):
+  return render(request, 'index.html',{})
+
+def nosotros(request):
+  return render(request, 'nosotros.html', {})
+
+def servicios(request):
+  return render(request, 'servicios.html', {})
+
+def blog(request):
+  return render(request, 'blog.html', {})
+
+def contacto(request):
+  return render(request, 'contacto.html', {})
